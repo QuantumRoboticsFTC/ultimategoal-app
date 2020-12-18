9dc52c3d7a303878c383d0c41faaf17d098909f5
Radu Pogonariu <radu@qrobotics.eu>
2020-12-18 17:15:00 +0100

    Initial boilerplate commit

---
+++ TeamCode/src/main/java/eu/qrobotics/ultimategoal/teamcode/subsystems/Subsystem.java
@@ -0,0 +1,10 @@
+package eu.qrobotics.ultimategoal.teamcode.subsystems;
+
+
+public interface Subsystem {
+    /**
+     * Run control code (e.g., read sensors and update motors)
+     * TODO: Return telemetry.
+     */
+    void update();
+}
